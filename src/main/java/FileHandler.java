3a4dffe30b239b8e964e34799fa77c9b8553b369
Risshi Naavaal <risshi@devsec.com>
2026-02-10 09:39:21 -0500

    add: file handler test file and add fields, constructor, and methods for file handler class.

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -1,2 +1,39 @@
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.Stream;
+
 public class FileHandler {
+    private final Cipher cipher;
+    private String result;
+    private List<String> files;
+    private static final String DATA_DIRECTORY = "data";
+
+    public FileHandler(Cipher cipher) {
+        this.cipher = cipher;
+        this.result = null;
+        this.files = new ArrayList<>();
+        loadAvailableFiles();
+    }
+
+    private void loadAvailableFiles() {
+
+    }
+
+    // In the case where a filename is provided
+    public void handleFile(String filename) {
+
+    }
+
+    // In the case where a file name is not provided
+    public String listFiles() {
+        return "";
+    }
+
+    public String getResult() {
+        return "";
+    }
+
+    private String readFileAsString(String filename) throws Exception {
+        return "";
+    }
 }
