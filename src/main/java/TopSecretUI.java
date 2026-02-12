d9fc3d20c47a50825635e642a6406e11d480ce91
diyamahendravadi <diyamahendravadi@gmail.com>
2026-02-12 15:32:58 -0500

    added comments

--- src/main/java/TopSecretUI.java
+++ src/main/java/TopSecretUI.java
@@ -8,25 +8,25 @@ public class TopSecretUI {
 
     public void run(String[] args) {
         if (args.length == 0) {
-            if (controller != null) controller.listFiles();
+            if (controller != null) controller.listFiles();// call list files if there is no input from user so they get  alist of files
             // if there is no arguements lsit the files
             this.a = 0;
         } else if (args.length == 1) {
-            if (controller != null) controller.displayFiles(args[0], null);
+            if (controller != null) controller.displayFiles(args[0], null);// call display files with out a key file if they only input one
             //display the file with the default key
             this.a = 1;
         } else if (args.length == 2) {
-            if (controller != null) controller.displayFiles(args[0], args[1]);
+            if (controller != null) controller.displayFiles(args[0], args[1]);//call display files with the new key and the file
             //display the file with the new key
             this.a = 2;
         } else {
-            if (controller != null) controller.handleTooManyArgs();
+            if (controller != null) controller.handleTooManyArgs();// do the handle too many if they put in over two arguemnts
             //if there is too many call and error and than print the usage instructions
             printUsage();
             this.a = 3;
         }
     }
-
+    // added the instructions for how this works
     private void printUsage() {
         System.out.println("java TopSecret -> list available files");
         System.out.println("java TopSecret [num]  -> view file [num] and decipher using default key");
