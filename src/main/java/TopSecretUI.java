7e4ef3c1a195854eb7c77f68287d3d23e35c2efb
diyamahendravadi <diyamahendravadi@gmail.com>
2026-02-12 10:29:31 -0500

    added more comments

--- src/main/java/TopSecretUI.java
+++ src/main/java/TopSecretUI.java
@@ -1,6 +1,6 @@
 public class TopSecretUI {
     private ProgramController controller;
-    public int a; // Tracker for testing logic branches
+    public int a; // Tracker for testing
 
     public TopSecretUI(ProgramController controller) {
         this.controller = controller;
@@ -8,26 +8,28 @@ public class TopSecretUI {
 
     public void run(String[] args) {
         if (args.length == 0) {
-            // Branch 0: No Arguments
             if (controller != null) controller.listFiles();
+            // if there is no arguements lsit the files
             this.a = 0;
         } else if (args.length == 1) {
-            // Branch 1: One Argument (e.g., "01")
             if (controller != null) controller.displayFiles(args[0], null);
+            //display the file with the default key
             this.a = 1;
         } else if (args.length == 2) {
-            // Branch 2: Two Arguments (e.g., "01", "cipher.key")
             if (controller != null) controller.displayFiles(args[0], args[1]);
+            //display the file with the new key
             this.a = 2;
         } else {
-            // Branch 3: Error/Too Many
             if (controller != null) controller.handleTooManyArgs();
+            //if there is too many call and error and than print the usage instructions
             printUsage();
             this.a = 3;
         }
     }
 
     private void printUsage() {
-        System.out.println("Usage instructions displayed...");
+        System.out.println("java TopSecret -> list available files");
+        System.out.println("java TopSecret [num]  -> view file [num] and decipher using default key");
+        System.out.println("java topsecret [num] [keyfile] -> view file [num] and decipher using [keyfile]");
     }
 }
